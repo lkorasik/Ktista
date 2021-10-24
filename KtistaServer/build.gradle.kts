814ae3c86932c138c77e3bc59a7ddf0527b61290
Lev <lkorasik83@gmail.com>
2021-10-24 15:17:19 +0500

    Create project with Ktor

---
+++ KtistaServer/build.gradle.kts
@@ -0,0 +1,33 @@
+import org.jetbrains.kotlin.gradle.tasks.KotlinCompile
+
+plugins {
+    kotlin("jvm") version "1.5.10"
+    application
+}
+
+group = "com.lkorasik"
+version = "0.1"
+
+repositories {
+    jcenter()
+    mavenCentral()
+}
+
+dependencies {
+    testImplementation(kotlin("test"))
+    implementation("io.ktor:ktor-server-netty:1.5.2")
+    implementation("io.ktor:ktor-html-builder:1.5.2")
+    implementation("org.jetbrains.kotlinx:kotlinx-html-jvm:0.7.2")
+}
+
+tasks.test {
+    useJUnit()
+}
+
+tasks.withType<KotlinCompile>() {
+    kotlinOptions.jvmTarget = "1.8"
+}
+
+application {
+    mainClass.set("ServerKt")
+}
---
+++ KtistaServer/src/main/kotlin/Server.kt
@@ -0,0 +1,30 @@
+import io.ktor.application.call
+import io.ktor.html.respondHtml
+import io.ktor.http.HttpStatusCode
+import io.ktor.routing.get
+import io.ktor.routing.routing
+import io.ktor.server.engine.embeddedServer
+import io.ktor.server.netty.Netty
+import kotlinx.html.*
+
+fun HTML.index() {
+    head {
+        title("Hello from Ktor!")
+    }
+    body {
+        div {
+            +"Hello from Ktor"
+        }
+    }
+}
+
+fun main() {
+    embeddedServer(Netty, port = 12364, host = "192.168.0.231") {
+        routing {
+            get("/") {
+                println("Connected")
+                call.respondHtml(HttpStatusCode.OK, HTML::index)
+            }
+        }
+    }.start(wait = true)
+}
