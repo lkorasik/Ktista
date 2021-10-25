6a560b7eb76e94a28416f4789e66190a68274eff
Lev <lkorasik83@gmail.com>
2021-10-25 13:59:01 +0500

    Add miock for button

--- KtistaClient/app/src/main/java/com/lkorasik/ktistaclient/ui/SettingsFragment.kt
+++ KtistaClient/app/src/main/java/com/lkorasik/ktistaclient/ui/SettingsFragment.kt
@@ -12,6 +12,7 @@ class SettingsFragment: Fragment() {
     private lateinit var lastNameEditText: EditText
     private lateinit var emailEditText: EditText
     private lateinit var nicknameEditText: EditText
+    private lateinit var changePasswordButton: Button
 
     override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?): View? {
         val view = inflater.inflate(R.layout.fragment_settings, container, false)
@@ -26,6 +27,7 @@ class SettingsFragment: Fragment() {
         initLastNameView(view)
         initEmailView(view)
         initNicknameView(view)
+        initChangePasswordView(view)
     }
 
     private fun initFirstNameView(view: View){
@@ -73,6 +75,22 @@ class SettingsFragment: Fragment() {
         nicknameEditText = root.findViewById<EditText>(R.id.field_value).apply {
             hint = getString(R.string.settings_nickname_name)
             inputType = InputType.TYPE_CLASS_TEXT
+        }
+    }
+
+    private fun initChangePasswordView(view: View){
+        val root = view.findViewById<View>(R.id.password)
+
+        root.findViewById<TextView>(R.id.field_title).apply{
+            text = getString(R.string.settings_password)
+        }
+        changePasswordButton = root.findViewById<Button>(R.id.button).apply {
+            text = getString(R.string.settings_change)
+
+            setOnClickListener {
+                Toast.makeText(view.context, "Mock. I fill it in next task", Toast.LENGTH_LONG).show()
+                //TODO("Realize change password")
+            }
         }
     }
 }
