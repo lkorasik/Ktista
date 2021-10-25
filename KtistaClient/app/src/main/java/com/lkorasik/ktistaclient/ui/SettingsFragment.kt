0eecd5c7b836f6c69828c31d32236f4a607c7cec
Lev <lkorasik83@gmail.com>
2021-10-25 13:13:28 +0500

    Clean

--- KtistaClient/app/src/main/java/com/lkorasik/ktistaclient/ui/SettingsFragment.kt
+++ KtistaClient/app/src/main/java/com/lkorasik/ktistaclient/ui/SettingsFragment.kt
@@ -1,29 +1,16 @@
 package com.lkorasik.ktistaclient.ui
 
-import android.content.Context
-import android.opengl.ETC1
 import android.os.Bundle
-import android.provider.ContactsContract
-import android.util.Log
 import android.view.*
 import android.widget.*
 import androidx.fragment.app.Fragment
 import com.lkorasik.ktistaclient.R
-import java.util.*
 
 class SettingsFragment: Fragment() {
     private lateinit var firstNameEditText: EditText
     private lateinit var lastNameEditText: EditText
     private lateinit var emailEditText: EditText
     private lateinit var nicknameEditText: EditText
-
-    override fun onAttach(context: Context) {
-        super.onAttach(context)
-    }
-
-    override fun onCreate(savedInstanceState: Bundle?) {
-        super.onCreate(savedInstanceState)
-    }
 
     override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?): View? {
         val view = inflater.inflate(R.layout.fragment_settings, container, false)
@@ -69,41 +56,5 @@ class SettingsFragment: Fragment() {
         nicknameEditText = root.findViewById<EditText>(R.id.field_value).apply {
             hint = "Your nickname"
         }
-    }
-
-    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
-        super.onViewCreated(view, savedInstanceState)
-    }
-
-    override fun onActivityCreated(savedInstanceState: Bundle?) {
-        super.onActivityCreated(savedInstanceState)
-    }
-
-    override fun onStart() {
-        super.onStart()
-    }
-
-    override fun onResume() {
-        super.onResume()
-    }
-
-    override fun onPause() {
-        super.onPause()
-    }
-
-    override fun onStop() {
-        super.onStop()
-    }
-
-    override fun onDestroyView() {
-        super.onDestroyView()
-    }
-
-    override fun onDestroy() {
-        super.onDestroy()
-    }
-
-    override fun onDetach() {
-        super.onDetach()
     }
 }
