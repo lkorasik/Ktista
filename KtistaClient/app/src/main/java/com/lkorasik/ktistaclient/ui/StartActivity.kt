520a5a47b49509114ca83fc45b743593870b1638
Lev <lkorasik83@gmail.com>
2021-10-28 10:27:12 +0500

    И еще одну лишнюю строчку убрал

--- KtistaClient/app/src/main/java/com/lkorasik/ktistaclient/ui/StartActivity.kt
+++ KtistaClient/app/src/main/java/com/lkorasik/ktistaclient/ui/StartActivity.kt
@@ -9,7 +9,6 @@ import com.lkorasik.ktistaclient.ui.login.LoginFragment
 
 class StartActivity: AppCompatActivity() {
     private lateinit var binding: ActivityStartBinding
-    private lateinit var fragmentContainer: FrameLayout
     private val fragmentContainerId = R.id.fragment_container
 
     override fun onCreate(savedInstanceState: Bundle?) {
