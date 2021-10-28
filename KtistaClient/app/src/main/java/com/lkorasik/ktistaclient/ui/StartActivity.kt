473f1f04da92dd7d6fa424598f823c4f641ed125
Lev <lkorasik83@gmail.com>
2021-10-28 08:43:51 +0500

    Add start activity

--- KtistaClient/app/src/main/java/com/lkorasik/ktistaclient/ui/StartActivity.kt
+++ KtistaClient/app/src/main/java/com/lkorasik/ktistaclient/ui/StartActivity.kt
@@ -1,16 +1,19 @@
 package com.lkorasik.ktistaclient.ui
 
 import android.os.*
-import android.widget.FrameLayout
+import android.widget.*
 import androidx.appcompat.app.AppCompatActivity
 import com.lkorasik.ktistaclient.R
+import com.lkorasik.ktistaclient.databinding.ActivityStartBinding
 
 class StartActivity: AppCompatActivity() {
+    private lateinit var binding: ActivityStartBinding
     private lateinit var fragmentContainer: FrameLayout
 
-    override fun onCreate(savedInstanceState: Bundle?, persistentState: PersistableBundle?) {
-        super.onCreate(savedInstanceState, persistentState)
-        setContentView(R.layout.activity_start)
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        binding = ActivityStartBinding.inflate(layoutInflater)
+        setContentView(binding.root)
 
         fragmentContainer = findViewById(R.id.fragment_container)
     }
