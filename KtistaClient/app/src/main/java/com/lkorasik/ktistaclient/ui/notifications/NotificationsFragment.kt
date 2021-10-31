3b7998add21da1a14ba72ac04d8db63599c1d262
Lev <lkorasik83@gmail.com>
2021-10-31 20:32:19 +0500

    Добавил view для отображения подписчиков

--- KtistaClient/app/src/main/java/com/lkorasik/ktistaclient/ui/notifications/NotificationsFragment.kt
+++ KtistaClient/app/src/main/java/com/lkorasik/ktistaclient/ui/notifications/NotificationsFragment.kt
@@ -9,7 +9,8 @@ import androidx.fragment.app.Fragment
 import androidx.lifecycle.Observer
 import androidx.lifecycle.ViewModelProvider
 import com.lkorasik.ktistaclient.R
-import com.lkorasik.ktistaclient.databinding.FragmentNotificationsBinding
+import com.lkorasik.ktistaclient.databinding.*
+import com.lkorasik.ktistaclient.ui.*
 
 class NotificationsFragment : Fragment() {
 
@@ -20,16 +21,15 @@ class NotificationsFragment : Fragment() {
     // onDestroyView.
     private val binding get() = _binding!!
 
-    override fun onCreateView(
-        inflater: LayoutInflater,
-        container: ViewGroup?,
-        savedInstanceState: Bundle?
-    ): View? {
+    override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?): View {
         notificationsViewModel =
             ViewModelProvider(this).get(NotificationsViewModel::class.java)
 
         _binding = FragmentNotificationsBinding.inflate(inflater, container, false)
         val root: View = binding.root
+
+        binding.userSubscribe.fuUserInfo.tvUserName.text = "Lev"
+        binding.userSubscribe.fuUserInfo.tvNickname.text = "sololev"
 
         return root
     }
