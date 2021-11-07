d715454575a6e1a3fc9878c9d8b4ddfd3fb6f202
Lev <lkorasik83@gmail.com>
2021-11-07 19:39:50 +0500

    Update AddPostActivity.kt

--- KtistaClient/app/src/main/java/com/lkorasik/ktistaclient/ui/AddPostActivity.kt
+++ KtistaClient/app/src/main/java/com/lkorasik/ktistaclient/ui/AddPostActivity.kt
@@ -1,13 +1,24 @@
 package com.lkorasik.ktistaclient.ui
 
+import android.app.*
+import android.app.AlertDialog
 import android.content.*
 import android.graphics.Bitmap
 import android.os.*
 import android.provider.MediaStore
 import android.widget.*
-import androidx.appcompat.app.AppCompatActivity
 import com.lkorasik.ktistaclient.R
 import com.lkorasik.ktistaclient.databinding.ActivityAddPostBinding
+import androidx.core.app.ActivityCompat.startActivityForResult
+
+import android.content.Intent
+
+import android.content.DialogInterface
+import android.database.Cursor
+import androidx.appcompat.app.*
+import android.graphics.BitmapFactory
+import android.net.Uri
+
 
 class AddPostActivity: AppCompatActivity() {
     private lateinit var image: ImageView
@@ -25,29 +36,58 @@ class AddPostActivity: AppCompatActivity() {
         image = binding.ivPostPhoto
         description = binding.etDescription
 
-        dispatchTakePictureIntent()
-
         val draw = getDrawable(R.drawable.outrun_vaporwave_hd_wallpaper_preview)
         image.setImageDrawable(draw)
+
+        chooseImage(this)
+    }
+
+    private fun chooseImage(context: Context) {
+        val optionsMenu = arrayOf<CharSequence>("Take Photo", "Choose from Gallery", "Exit")
+        val builder: AlertDialog.Builder = AlertDialog.Builder(context)
+        builder.setItems(optionsMenu) { dialogInterface, i ->
+            when {
+                optionsMenu[i] == "Take Photo" -> {
+                    val takePicture = Intent(MediaStore.ACTION_IMAGE_CAPTURE)
+                    startActivityForResult(takePicture, 0)
+                }
+                optionsMenu[i] == "Choose from Gallery" -> {
+                    val pickPhoto = Intent(Intent.ACTION_PICK, MediaStore.Images.Media.EXTERNAL_CONTENT_URI)
+                    startActivityForResult(pickPhoto, 1)
+                }
+                optionsMenu[i] == "Exit" -> {
+                    dialogInterface.dismiss()
+                }
+            }
+        }
+        builder.show()
     }
 
     override fun onActivityResult(requestCode: Int, resultCode: Int, data: Intent?) {
         super.onActivityResult(requestCode, resultCode, data)
 
-        if (requestCode == REQUEST_IMAGE_CAPTURE && resultCode == RESULT_OK) {
-            val imageBitmap = data?.extras?.get("data") as Bitmap
-            image.setImageBitmap(imageBitmap)
-        }
-    }
-
-    private val REQUEST_IMAGE_CAPTURE = 1
-
-    private fun dispatchTakePictureIntent() {
-        val takePictureIntent = Intent(MediaStore.ACTION_IMAGE_CAPTURE)
-        try {
-            startActivityForResult(takePictureIntent, REQUEST_IMAGE_CAPTURE)
-        } catch (e: ActivityNotFoundException) {
-            // display error state to the user
+        if (resultCode != RESULT_CANCELED) {
+            when (requestCode) {
+                0 -> if (resultCode == RESULT_OK && data != null) {
+                    val selectedImage = data.extras!!["data"] as Bitmap?
+                    image.setImageBitmap(selectedImage)
+                }
+                1 -> if (resultCode == RESULT_OK && data != null) {
+                    val selectedImage: Uri? = data.data
+                    val filePathColumn = arrayOf(MediaStore.Images.Media.DATA)
+                    if (selectedImage != null) {
+                        val cursor: Cursor? =
+                            contentResolver.query(selectedImage, filePathColumn, null, null, null)
+                        if (cursor != null) {
+                            cursor.moveToFirst()
+                            val columnIndex: Int = cursor.getColumnIndex(filePathColumn[0])
+                            val picturePath: String = cursor.getString(columnIndex)
+                            image.setImageBitmap(BitmapFactory.decodeFile(picturePath))
+                            cursor.close()
+                        }
+                    }
+                }
+            }
         }
     }
 }
