8418ae71c6d59d253119bf7ed5a7ccdeae74c3b5
AlibabaRazboinik <korall.sofiya@mail.ru>
2021-11-29 12:52:39 +0500

    добавлена регистрация с ворнингами

---
+++ KtistaClient/app/src/main/java/com/lkorasik/ktistaclient/net/RegistrationStages.kt
@@ -0,0 +1,8 @@
+package com.lkorasik.ktistaclient.net
+
+enum class RegistrationStages {
+    INIT,
+    IN_PROGRESS,
+    SUCCESS,
+    FAIL
+}
--- KtistaClient/app/src/main/java/com/lkorasik/ktistaclient/ui/start/registration/RegistrationViewModel.kt
+++ KtistaClient/app/src/main/java/com/lkorasik/ktistaclient/ui/start/registration/RegistrationViewModel.kt
@@ -1,5 +1,34 @@
 package com.lkorasik.ktistaclient.ui.start.registration
 
+import android.util.Log
+import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
+import androidx.lifecycle.viewModelScope
+import com.lkorasik.ktistaclient.net.RegistrationRequest
+import com.lkorasik.ktistaclient.net.RegistrationStages
+import com.lkorasik.ktistaclient.net.model.UserRegistrationRequest
+import kotlinx.coroutines.launch
 
-class RegistrationViewModel: ViewModel()
+class RegistrationViewModel: ViewModel(){
+    companion object{
+        val LOG_TAG = this::class.java.canonicalName
+    }
+
+    private val registrationRequest: RegistrationRequest = RegistrationRequest({
+        inProgress.value = RegistrationStages.SUCCESS
+        Log.i(LOG_TAG, "Request was success ${inProgress.value}")
+    }, {
+        inProgress.value = RegistrationStages.FAIL
+        Log.i(LOG_TAG, "Request was fail ${inProgress.value}")
+    })
+
+    val inProgress = MutableLiveData(RegistrationStages.INIT)
+
+    fun registerUser(nickname: String, password: String, email: String){
+        viewModelScope.launch {
+            inProgress.value = RegistrationStages.IN_PROGRESS
+            Log.i(LOG_TAG, "Start request ${inProgress.value}")
+            registrationRequest.registerUser(UserRegistrationRequest(nickname, password, email))
+        }
+    }
+}
