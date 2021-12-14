ce880b67f19b780cb80c86941b3bfdae775ad896
Lev <lkorasik83@gmail.com>
2021-12-14 18:54:58 +0500

    Add photo loading from gallery

--- KtistaClient/app/src/main/java/com/lkorasik/ktistaclient/ui/profile/ProfileViewModel.kt
+++ KtistaClient/app/src/main/java/com/lkorasik/ktistaclient/ui/profile/ProfileViewModel.kt
@@ -5,6 +5,21 @@ import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
 import com.lkorasik.ktistaclient.ui.models.PostModel
 import com.lkorasik.ktistaclient.ui.TestDataClass
+import android.graphics.BitmapFactory
+
+import android.R
+
+import android.provider.MediaStore
+
+import android.content.Intent
+
+import androidx.core.app.ActivityCompat.startActivityForResult
+
+import android.os.Bundle
+
+import android.app.Activity
+import android.view.View
+
 
 class ProfileViewModel : ViewModel() {
     private val mutablePostsData: MutableLiveData<ArrayList<PostModel>> = MutableLiveData()
