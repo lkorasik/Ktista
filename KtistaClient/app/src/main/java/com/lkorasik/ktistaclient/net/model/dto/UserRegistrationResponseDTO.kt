030343dd8b2836606b67187d1ba851db3f1c4e1e
Lev <lkorasik83@gmail.com>
2021-12-20 20:27:51 +0500

    Fix

--- KtistaClient/app/src/main/java/com/lkorasik/ktistaclient/net/model/dto/UserRegistrationResponseDTO.kt
+++ KtistaClient/app/src/main/java/com/lkorasik/ktistaclient/net/model/dto/UserRegistrationResponseDTO.kt
@@ -1,3 +1,3 @@
 package com.lkorasik.ktistaclient.net.model.dto
 
-data class UserRegistrationResponseDTO(var id: Int, var email: String, var userStatus: String, var createdAt: String)
+data class UserRegistrationResponseDTO(var id: Int)
