8b0512df83dfc51c9ba8be16cb16dbf2d66ca12e
obduvan <obduvann@gmail.com>
2022-01-31 19:35:04 +0500

    сохранение токена в sharedPreferences

--- KtistaClient/app/src/main/java/com/lkorasik/ktistaclient/net/repository/UserRepository.kt
+++ KtistaClient/app/src/main/java/com/lkorasik/ktistaclient/net/repository/UserRepository.kt
@@ -1,19 +1,20 @@
 package com.lkorasik.ktistaclient.net.repository
 
+import android.content.Context
 import com.lkorasik.ktistaclient.net.core.RequestContext
 import com.lkorasik.ktistaclient.net.model.HeadersKeys
 import com.lkorasik.ktistaclient.net.model.dto.UserLoginRequestDTO
 import okhttp3.Headers
 import retrofit2.Response
 
-class UserRepository {
+class UserRepository (private val context: Context) {
     fun login(user: UserLoginRequestDTO): Response<Void?> {
         val response = RequestContext.API.login(user).execute()
 
-        JwtRepository.extractToken(response)
+        JwtRepository.saveToken(response, context)
 
         return response
         //TODO(Handle errors. Response you can get from execute())
     }
-}
 
+}
