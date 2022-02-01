39c7223cf6c9905f0963e3207bc598f5579bb41a
obduvan <obduvann@gmail.com>
2022-02-01 18:05:21 +0500

    jwt сохранил в preferences

--- KtistaClient/app/src/main/java/com/lkorasik/ktistaclient/net/repository/JwtRepository.kt
+++ KtistaClient/app/src/main/java/com/lkorasik/ktistaclient/net/repository/JwtRepository.kt
@@ -1,14 +1,43 @@
 package com.lkorasik.ktistaclient.net.repository
 
+import android.content.Context
+import android.content.SharedPreferences
 import com.lkorasik.ktistaclient.net.model.HeadersKeys
 import retrofit2.Response
 
 object JwtRepository {
-    var jwt: String = ""
-        private set
-        get
 
-    fun <T> extractToken(response: Response<T>) {
-        jwt = response.headers()[HeadersKeys.AUTHORIZATION].toString().split(" ")[1]
+    private var jwtPreferences: SharedPreferences? = null
+    private const val JWT_PREFERENCES = "JWT_PREFERENCES"
+    private const val JWT_ACCESS = "JWT_ACCESS"
+    private const val JWT_REFRESH = "JWT_REFRESH"
+
+    var jwtAccess: String = ""
+        private set
+        get() {
+            if (field.isEmpty()) {
+                field = getToken(JWT_ACCESS) ?: ""
+            }
+            return field
+        }
+
+    fun <T> saveToken(response: Response<T>, context: Context) {
+        jwtAccess = extractToken(response)
+        jwtPreferences = context.getSharedPreferences(JWT_PREFERENCES, Context.MODE_PRIVATE)
+
+        jwtPreferences?.let {
+            with(it.edit()) {
+                putString(JWT_ACCESS, jwtAccess)
+                apply()
+            }
+        }
     }
+
+    private fun getToken(nameToken: String): String? {
+        return jwtPreferences?.getString(nameToken, "")
+    }
+
+    private fun <T> extractToken(response: Response<T>): String =
+        response.headers()[HeadersKeys.AUTHORIZATION].toString().split(" ")[1]
+
 }
